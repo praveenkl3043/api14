df4ee1e21772df4cf7af5bf580130d8ccb661697
praveenkl3043 <lalpraveenkl@gmail.com>
2022-01-27 01:12:25 +0530

    api14

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btn=document.getElementsByClassName("btn btn-primary");
 const result=document.getElementsByClassName("data");
 
-const url="https://api.github.com/users/praveenkl3043";
+const url="https://coronavirus.m.pipedream.net/";
 
 btn[0].addEventListener("click",async function (){
 
@@ -12,11 +12,23 @@ btn[0].addEventListener("click",async function (){
          let objet=await res.json();
          console.log(objet)
          result[0].innerHTML=`
-         <h6 class="login">Login:<span>${objet.login}<span></h6>
-         <h6 class="id">id:<span>${objet.id}</span></h6>
-         <h6 class="repo">public repos:<span>${objet.public_repos}</span></h6>
-         <h6 class="githubprofile">Githubprofile:<span>${objet.html_url}</span></h6>
-         
+         <h2 class="global">Global</h2> 
+         <h6 class="confirmed">confirmed:<span>${objet.summaryStats.global.confirmed}</span></h6>
+         <h6 class="death">deaths:<span>${objet.summaryStats.global.deaths}</span></h6>
+         <h6 class="recovered">recovered:<span>${objet.summaryStats.global.recovered}</span></h6>
+         <h2 class="china">China</h2>
+         <h6 class="confirmedn">confirmed:<span>${objet.summaryStats.china.confirmed}</span></h6>
+         <h6 class="deathsn">deaths:<span>${objet.summaryStats.china.deaths}</span></h6>
+         <h6 class="recoveredn">recovered:<span>${objet.summaryStats.china.recovered}</span></h6>
+         <h2 class="nonchina">Non China</h2>
+         <h6 class="confirmedc">confirmed:<span>${objet.summaryStats.nonChina.confirmed}</span></h6>
+         <h6 class="recoveredc">deaths:<span>${objet.summaryStats.nonChina.deaths}</span></h6>
+         <h6 class="deathsc">recovered:<span>${objet.summaryStats.nonChina.recovered}</span></h6>
+         <h2 class="lastupdated">Last Updated</h2>
+         <h6 class="updated">updated:<span>${objet.cache.lastUpdated}</span></h6>
+         <h2 class="expires">EXPIRES IN</h2>
+         <h6 class="updated">expiresin:<span>${objet.cache.expires}</span></h6>
+
          `
      }catch (error){
          console.log(error);
